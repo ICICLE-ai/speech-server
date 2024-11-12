771660bad92606511a7d32ed1ce62ce0d825f12d
vishalsunder <sundervishal26@gmail.com>
2024-11-12 16:32:41 -0500

    restarting whisper

--- archive.sh
+++ archive.sh
@@ -7,6 +7,15 @@ torch-model-archiver \
     --handler whisperx_handler.py \
     --extra-files config_wx.json \
     --export-path model_store
+#model_name=my_asr
+#rm -vrf model_store/$model_name.mar
+#torch-model-archiver \
+#    --model-name $model_name \
+#    --version 1.0 \
+#    --model-file my_model.py \
+#    --handler my_handler.py \
+#    --extra-files "models.py,encoders.py,util.py,hc_ctc_all.yaml" \
+#    --export-path model_store
 #model_name=nemo_asr
 #rm -vrf model_store/$model_name.mar
 #torch-model-archiver \
--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #export TMPDIR=/local/scratch
 #torchserve --start --ncs --disable-token-auth  --ts-config config.properties --model-store model_store --models nemo_asr=nemo_asr.mar
 torchserve --start --ncs --disable-token-auth --ts-config config.properties --model-store model_store --models whisperx_asr=whisperx_asr.mar
+#torchserve --start --ncs --disable-token-auth --ts-config config.properties --model-store model_store --models my_asr=my_asr.mar
 #torchserve --start --ncs --disable-token-auth  --model-store model_store --models fasterWhisper_asr=fasterWhisper_asr.mar
